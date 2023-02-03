3448ca80a6fb3981ffa1ccd1a1e5ef4338a26901
E Luckie <eluckie.d@gmail.com>
2023-02-03 11:25:05 -0700

    add test to calculate Jupiter years passed based on Earth years passed

--- __tests__/past-bday.test.js
+++ __tests__/past-bday.test.js
@@ -41,5 +41,16 @@ describe("YearsPassed", () => {
     expect(yearsPassed.venusYears).toEqual(yearDifference * (1 / .62));
     expect(yearsPassed.marsYears).toEqual(yearDifference / 1.88);
   });
-  
+
+  test("should correctly calculate years passed in Mercury, Venus, Mars and Jupiter years by using the years passed on Earth", () => {
+    const currentAge = 30;
+    const previousAge = 20;
+    const yearDifference = (currentAge - previousAge);
+    const yearsPassed = new YearsPassed(currentAge, previousAge);
+    expect(yearsPassed.earthYears).toEqual(yearDifference);
+    expect(yearsPassed.mercuryYears).toEqual(yearDifference * (1 / .24));
+    expect(yearsPassed.venusYears).toEqual(yearDifference * (1 / .62));
+    expect(yearsPassed.marsYears).toEqual(yearDifference / 1.88);
+    expect(yearsPassed.jupiterYears).toEqual(yearDifference / 11.86);
+  });
 });
