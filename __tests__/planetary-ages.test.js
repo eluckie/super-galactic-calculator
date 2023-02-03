899820b887893258b4ae282b68cde765695e5579
E Luckie <eluckie.d@gmail.com>
2023-02-03 11:43:20 -0700

    refactor PlanetaryAges to have yearsPassed() and remove YearsPassed constructor

--- __tests__/planetary-ages.test.js
+++ __tests__/planetary-ages.test.js
@@ -5,36 +5,43 @@ describe("PlanetaryAges", () => {
 
   test("should correctly create an object with user's age on Earth", () => {
     const person = new PlanetaryAges(30);
-    expect(person.earthAge).toEqual(30);
+    expect(person.earthYears).toEqual(30);
   });
 
   test("should correctly calculate user's age in Mercury years by using their age on Earth", () => {
     const person = new PlanetaryAges(30);
-    expect(person.earthAge).toEqual(30);
-    expect(person.mercuryAge).toEqual(30 * (1 / .24));
+    expect(person.earthYears).toEqual(30);
+    expect(person.mercuryYears).toEqual(30 * (1 / .24));
   });
 
   test("should correctly calculate user's age in Mercury and Venus years by using their age on Earth", () => {
     const person = new PlanetaryAges(30);
-    expect(person.earthAge).toEqual(30);
-    expect(person.mercuryAge).toEqual(30 * (1 / .24));
-    expect(person.venusAge).toEqual(30 * (1 / .62));
+    expect(person.earthYears).toEqual(30);
+    expect(person.mercuryYears).toEqual(30 * (1 / .24));
+    expect(person.venusYears).toEqual(30 * (1 / .62));
   });
 
   test("should correctly calculate user's age in Mercury, Venus, and Mars years by using their age on Earth", () => {
     const person = new PlanetaryAges(30);
-    expect(person.earthAge).toEqual(30);
-    expect(person.mercuryAge).toEqual(30 * (1 / .24));
-    expect(person.venusAge).toEqual(30 * (1 / .62));
-    expect(person.marsAge).toEqual(30 / 1.88);
+    expect(person.earthYears).toEqual(30);
+    expect(person.mercuryYears).toEqual(30 * (1 / .24));
+    expect(person.venusYears).toEqual(30 * (1 / .62));
+    expect(person.marsYears).toEqual(30 / 1.88);
   });
 
   test("should correctly calculate user's age in Mercury, Venus, Mars, and Jupiter years by using their age on Earth", () => {
     const person = new PlanetaryAges(30);
-    expect(person.earthAge).toEqual(30);
-    expect(person.mercuryAge).toEqual(30 * (1 / .24));
-    expect(person.venusAge).toEqual(30 * (1 / .62));
-    expect(person.marsAge).toEqual(30 / 1.88);
+    expect(person.earthYears).toEqual(30);
+    expect(person.mercuryYears).toEqual(30 * (1 / .24));
+    expect(person.venusYears).toEqual(30 * (1 / .62));
+    expect(person.marsYears).toEqual(30 / 1.88);
     expect(person.jupiterYears).toEqual(30 / 11.86);
+  });
+
+  test("should correctly calculate how many Earth years have passed since a previous birthday", () => {
+    const currentAge = 30;
+    const previousAge = 20;
+    const person = new PlanetaryAges(currentAge);
+    expect(person.yearsPassed()).toEqual(currentAge - previousAge);
   });
 });
