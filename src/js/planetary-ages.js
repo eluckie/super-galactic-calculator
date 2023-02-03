5bd9f30b21d8ee021086b537358056e7faffe056
E Luckie <eluckie.d@gmail.com>
2023-02-03 10:16:27 -0700

    pass test to calculate user's Jupiter and Mars age

--- src/js/planetary-ages.js
+++ src/js/planetary-ages.js
@@ -3,5 +3,7 @@ export default class PlanetaryAges {
     this.earthAge = earthAge;
     this.mercuryAge = earthAge * (1 / .24);
     this.venusAge = earthAge * (1 / .62);
+    this.marsAge = earthAge / 1.88;
+    this.jupiterAge = earthAge / 11.86;
   }
 }
