5976f83cdf63b20f9da28a38d38bf8bad9f065bd
E Luckie <eluckie.d@gmail.com>
2023-02-03 10:55:13 -0700

    add test to create new object to calculate years passed since a previous age

---
+++ __tests__/past-bday.test.js
@@ -0,0 +1,15 @@
+import { TestScheduler } from "jest";
+import PlanetaryAges from "../src/js/planetary-ages.js";
+import YearsPassed from "../src/js/past-bday.js";
+
+describe("YearsPassed", () => {
+
+  test("should correctly create an object that calculates how many Earth years have passed since a previous birthday", () => {
+    const currentAge = 30;
+    const previousAge = 20;
+    const yearDifference = (currentAge - previousAge);
+    const yearsPassed = new YearsPassed(yearDifference);
+    expect(yearsPassed.earthYears).toEqual(yearDifference);
+  });
+
+});
