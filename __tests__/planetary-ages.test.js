15f48fd553838f722ddc84c7c5129deba6b0ea87
E Luckie <eluckie.d@gmail.com>
2023-02-03 09:59:13 -0700

    adjust math used to calculate Mercury age and add test to calculate Venus age

--- __tests__/planetary-ages.test.js
+++ __tests__/planetary-ages.test.js
@@ -11,6 +11,13 @@ describe("PlanetaryAges", () => {
   test("should correctly calculate user's age in Mercury years by using their age on Earth", () => {
     const person = new PlanetaryAges(30);
     expect(person.earthAge).toEqual(30);
-    expect(person.mercuryAge).toEqual((30 * .24));
+    expect(person.mercuryAge).toEqual(30 * (1 / .24));
+  });
+
+  test("should correctly calculate user's age in Mercury and Venus years by using their age on Earth", () => {
+    const person = new PlanetaryAges(30);
+    expect(person.earthAge).toEqual(30);
+    expect(person.mercuryAge).toEqual(30 * (1 / .24));
+    expect(person.venusAge).toEqual(30 * (1 / .62));
   });
 });
