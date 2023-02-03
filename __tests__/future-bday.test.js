f38ce9fecbcf853584029765ebd7c0c803972eb7
E Luckie <eluckie.d@gmail.com>
2023-02-03 12:21:59 -0700

    add test to calculate how many years on each planet will need to pass until a future birthday by using the amount in Earth years

--- __tests__/future-bday.test.js
+++ __tests__/future-bday.test.js
@@ -10,4 +10,16 @@ describe("YearsToCome", () => {
     const yearsNeeded = new YearsToCome(currentAge, futureAge);
     expect(yearsNeeded.earthYears).toEqual(yearDifference);
   });
+
+  test("should correctly calculate years need to pass in Mercury, Venus, Mars and Jupiter years by using the years needed to pass on Earth", () => {
+    const currentAge = 20;
+    const futureAge = 30;
+    const yearDifference = (futureAge - currentAge);
+    const yearsNeeded = new YearsToCome(currentAge, futureAge);
+    expect(yearsNeeded.earthYears).toEqual(yearDifference);
+    expect(yearsNeeded.mercuryYears).toEqual(yearDifference * (1 / .24));
+    expect(yearsNeeded.venusYears).toEqual(yearDifference * (1 / .62));
+    expect(yearsNeeded.marsYears).toEqual(yearDifference / 1.88);
+    expect(yearsNeeded.jupiterYears).toEqual(yearDifference / 11.86);
+  });
 });
