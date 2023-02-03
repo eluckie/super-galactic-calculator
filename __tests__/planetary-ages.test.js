f14417c3ebbacf97146e41f7d3b1e3b46619e373
E Luckie <eluckie.d@gmail.com>
2023-02-03 09:44:29 -0700

    add test to calculate user's Mercury age

--- __tests__/planetary-ages.test.js
+++ __tests__/planetary-ages.test.js
@@ -8,4 +8,9 @@ describe("PlanetaryAges", () => {
     expect(person.earthAge).toEqual(30);
   });
 
+  test("should correctly calculate user's age in Mercury years by using their age on Earth", () => {
+    const person = new PlanetaryAges(30);
+    expect(person.earthAge).toEqual(30);
+    expect(person.mercuryAge).toEqual((30 * .24));
+  });
 });
