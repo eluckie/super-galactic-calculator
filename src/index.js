367eb2a38149cd8c22a735c363a4b1a770d1adbf
E Luckie <eluckie.d@gmail.com>
2023-02-03 16:21:01 -0700

    add reset button to clear screen & reset form

--- src/index.js
+++ src/index.js
@@ -14,23 +14,30 @@ function getPlanetaryAges(event) {
   const allPlanetAges = Object.values(planetAges);
   const mercuryP = document.createElement("p");
   const venusP = document.createElement("p");
-  const earthP = document.createElement("p");
+  const earthH5 = document.createElement("h5");
   const marsP = document.createElement("p");
   const jupiterP = document.createElement("p");
-  mercuryP.append("Your age on Mercury is " + allPlanetAges[1] + " years");
-  venusP.append("Your age on Venus is " + allPlanetAges[2] + " years");
-  earthP.append("Your age on Earth is " + allPlanetAges[0] + " years");
-  marsP.append("Your age on Mars is " + allPlanetAges[3] + " years");
-  jupiterP.append("Your age on Jupiter is " + allPlanetAges[4] + " years");
+  mercuryP.append("Your age on Mercury is " + allPlanetAges[1] + " years old.");
+  venusP.append("Your age on Venus is " + allPlanetAges[2] + " years old.");
+  earthH5.append("Your age on Earth is " + allPlanetAges[0] + " years old.");
+  marsP.append("Your age on Mars is " + allPlanetAges[3] + " years old.");
+  jupiterP.append("Your age on Jupiter is " + allPlanetAges[4] + " years old.");
+  div.append(earthH5);
   div.append(mercuryP);
   div.append(venusP);
-  div.append(earthP);
   div.append(marsP);
   div.append(jupiterP);
   div.removeAttribute("class");
   document.getElementById("age-form").reset();
 }
 
+function resetScreen() {
+  const div = document.getElementById("ages-object");
+  div.innerText = null;
+  document.getElementById("age-form").reset();
+}
+
 window.addEventListener("load", function() {
   document.getElementById("age-form").addEventListener("submit", getPlanetaryAges);
+  document.getElementById("reset-button").addEventListener("click", resetScreen);
 });
