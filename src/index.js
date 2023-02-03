0ef8c1afea49f81f50b33b2dd2ff79677c4ecb8f
E Luckie <eluckie.d@gmail.com>
2023-02-03 16:01:51 -0700

    refactor getPlanetaryAges() so that DOM is only accessed once

--- src/index.js
+++ src/index.js
@@ -9,16 +9,23 @@ function getPlanetaryAges(event) {
   const currentAge = document.getElementById("current-age").value;
   const otherAge = document.getElementById("other-age").value;
   const planetAges = new PlanetaryAges(currentAge, otherAge);
-  const earth = planetAges.earthYears;
-  const mercury = planetAges.mercuryYears;
-  const venus = planetAges.venusYears;
-  const mars = planetAges.marsYears;
-  const jupiter = planetAges.jupiterYears;
-  document.getElementById("mercury-years").innerText = mercury;
-  document.getElementById("venus-years").innerText = venus;
-  document.getElementById("earth-years").innerText = earth;
-  document.getElementById("mars-years").innerText = mars;
-  document.getElementById("jupiter-years").innerText = jupiter;
+  const allPlanetAges = Object.values(planetAges);
+  const mercuryP = document.createElement("p");
+  const venusP = document.createElement("p");
+  const earthP = document.createElement("p");
+  const marsP = document.createElement("p");
+  const jupiterP = document.createElement("p");
+  const div = document.getElementById("ages-object");
+  mercuryP.append("Your age on Mercury is " + allPlanetAges[1] + " years");
+  venusP.append("Your age on Venus is " + allPlanetAges[2] + " years");
+  earthP.append("Your age on Earth is " + allPlanetAges[0] + " years");
+  marsP.append("Your age on Mars is " + allPlanetAges[3] + " years");
+  jupiterP.append("Your age on Jupiter is " + allPlanetAges[4] + " years");
+  div.append(mercuryP);
+  div.append(venusP);
+  div.append(earthP);
+  div.append(marsP);
+  div.append(jupiterP);
 }
 
 window.addEventListener("load", function() {
