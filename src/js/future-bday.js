029df64450f23281408fa098c995343693cb60d4
E Luckie <eluckie.d@gmail.com>
2023-02-03 12:22:45 -0700

    pass test to calculate how many years on each planet will need to pass until a future birthday by using the amount in Earth years

--- src/js/future-bday.js
+++ src/js/future-bday.js
@@ -2,5 +2,9 @@ export default class YearsToCome {
   constructor(currentAge, otherAge) {
     const earthYears = (otherAge - currentAge);
     this.earthYears = earthYears;
+    this.mercuryYears = earthYears * (1 / .24);
+    this.venusYears = earthYears * (1 / .62);
+    this.marsYears = earthYears / 1.88;
+    this.jupiterYears = earthYears/ 11.86;
   }
 }
