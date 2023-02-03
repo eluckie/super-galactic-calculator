101f74195fc871589663586e2a5c69d5ce4d230f
E Luckie <eluckie.d@gmail.com>
2023-02-03 15:17:53 -0700

    add basic ui functionality to getPlanetaryAges() when form is submitted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import PlanetaryAges from "./js/planetary-ages.js";
+// import YearsToCome from "./future-bday.js";
+
+function getPlanetaryAges(event) {
+  event.preventDefault();
+  const currentAge = document.getElementById("current-age").value;
+  const otherAge = document.getElementById("other-age").value;
+  const planetAges = new PlanetaryAges(currentAge, otherAge);
+  console.log(planetAges);
+}
+
+window.addEventListener("load", function() {
+  document.getElementById("age-form").addEventListener("submit", getPlanetaryAges);
+});
