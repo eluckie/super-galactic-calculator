13e925cf9124a0b1f217d2b21fdd729fc4ac7586
E Luckie <eluckie.d@gmail.com>
2023-02-03 16:17:48 -0700

    add .toFixed() to round ages to 2 digits

--- src/js/future-bday.js
+++ src/js/future-bday.js
@@ -2,9 +2,9 @@ export default class YearsToCome {
   constructor(currentAge, otherAge) {
     const adjustedAge = otherAge - currentAge;
     this.earthYears = adjustedAge;
-    this.mercuryYears = adjustedAge * (1 / .24);
-    this.venusYears = adjustedAge * (1 / .62);
-    this.marsYears = adjustedAge / 1.88;
-    this.jupiterYears = adjustedAge / 11.86;
+    this.mercuryYears = (adjustedAge * (1 / .24)).toFixed(2);
+    this.venusYears = (adjustedAge * (1 / .62)).toFixed(2);
+    this.marsYears = (adjustedAge / 1.88).toFixed(2);
+    this.jupiterYears = (adjustedAge / 11.86).toFixed(2);
   }
 }
