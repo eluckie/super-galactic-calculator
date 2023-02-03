72bc43aa44b682e6be3f9816756de9328e1bb5b9
E Luckie <eluckie.d@gmail.com>
2023-02-03 10:05:23 -0700

    pass test to correctly calculate Venus age

--- src/js/planetary-ages.js
+++ src/js/planetary-ages.js
@@ -1,5 +1,7 @@
 export default class PlanetaryAges {
   constructor(earthAge) {
     this.earthAge = earthAge;
+    this.mercuryAge = earthAge * (1 / .24);
+    this.venusAge = earthAge * (1 / .62);
   }
 }
