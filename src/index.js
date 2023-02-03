077d3bb93b00c017007dc738eff81a0fdec9bc0a
E Luckie <eluckie.d@gmail.com>
2023-02-03 15:46:32 -0700

    WIP: refactor getPlanetaryAges() & index.html to only access DOM once to display results

--- src/index.js
+++ src/index.js
@@ -9,7 +9,16 @@ function getPlanetaryAges(event) {
   const currentAge = document.getElementById("current-age").value;
   const otherAge = document.getElementById("other-age").value;
   const planetAges = new PlanetaryAges(currentAge, otherAge);
-  console.log(planetAges);
+  const earth = planetAges.earthYears;
+  const mercury = planetAges.mercuryYears;
+  const venus = planetAges.venusYears;
+  const mars = planetAges.marsYears;
+  const jupiter = planetAges.jupiterYears;
+  document.getElementById("mercury-years").innerText = mercury;
+  document.getElementById("venus-years").innerText = venus;
+  document.getElementById("earth-years").innerText = earth;
+  document.getElementById("mars-years").innerText = mars;
+  document.getElementById("jupiter-years").innerText = jupiter;
 }
 
 window.addEventListener("load", function() {
